0f0feecb5cdbf0f243509184a0e5cfae8ef37cfb
Ivan-r-jpg <matadormax23@gmail.com>
2026-05-03 22:23:27 +0300

    Додавання файлів 9-ї лабораторної роботи

---
+++ lab.9/lab.9.1.cs
@@ -0,0 +1,270 @@
+﻿using System; // Використання простору імен System
+
+namespace lab._9._1
+{
+    class Transport // Базовий клас
+    {
+        // Захищені поля, що доступні базовому класу та його нащадкам
+        protected double netProfit; // Поле "Чистий дохід"
+        protected double profit; // Поле "Дохід"
+        protected double costs; // Поле "Витрати"
+        protected string transportNumber; // Поле "Номер транспорту"
+        protected string transportBrand; // Поле "Марка транспорту"
+        
+        public virtual void Input() // Віртуальна функція для вводу базової інформації про транспорт
+        {
+            Console.Write("Введіть марку транспортного засобу: ");
+            transportBrand = Console.ReadLine();
+            Console.Write("Введіть номер транспорту: ");
+            transportNumber = Console.ReadLine();
+            profit = Program.DoubleInput(0, 1000000, "Введіть прибуток за поїздку (у грн.): ");
+            costs = Program.DoubleInput(0, profit, "Введіть витрати за поїздку (у грн.): ");
+        }
+
+        protected virtual void CalculateNetProfit() // Віртуальна функція для обчислення чистого прибутку
+        {
+            netProfit = profit - costs;
+        }
+        public virtual void Output() // Віртуальна функція для виводу базової інформації про транспорт
+        {
+            CalculateNetProfit();
+            Console.WriteLine("\n   Транспортний засіб '{0}' з номерами '{1}':\n", transportBrand, transportNumber);
+            if (netProfit >= 0) // Якщо збитку немає, то виводиться наступне повідомлення
+            {
+                Console.WriteLine("   Чистий прибуток за перевезення: {0:F2} грн.", netProfit);
+            }
+            else // Якщо є збиток
+            {
+                Console.WriteLine("   [УВАГА] - Перевезення збиткове! Збиток склав: {0:F2} грн.", Math.Abs(netProfit));
+            }
+        }
+    }
+
+    class FreightTransport : Transport // Клас для представлення вантажного автомобіля
+    {
+        // Приватні поля, що доступні тільки в даному класі
+        private double cargoWeight; // Поле "Вага товару"
+        private double cargoAssessment; // Поле "Оцінка товару"
+        private int cargoСondition; // Поле "Стан товару"
+        private int driveHours; // Поле "Кількість годин водія"
+        private double driverBonus; // Поле "Надбавка водію"
+        private double fine; // Поле "Штраф"
+        private int optimalCountOfHours; // Поле "Оптимальна кількість годин"
+
+        public override void Input() // Перевизначення методу для вводу інформації
+        {
+            base.Input(); // Виклик методу з базового класу для вводу базової інформації
+            cargoWeight = Program.DoubleInput(0, 1000000, "Введіть вагу вантажу, що перевозиться (кг.): ");
+            cargoAssessment = Program.DoubleInput(0, 1000000, "Введіть оцінку товару, що перевозиться (грн.): ");
+            driveHours = Program.IntegerInput(0, 500, "Введіть кількість годин, що тривало перевезення: ");
+        }
+
+        protected override void CalculateNetProfit() // Перевизначення методу для обчислення чистого прибутку
+        {
+            optimalCountOfHours = Program.rd.Next(5, 200); // Генерування випадкового значення оптимальної кількості годин
+            cargoСondition = Program.rd.Next(40, 100); // Генерування випадкового значення стану товару від 40% до 100%
+            if (cargoСondition > 72 && driveHours <= optimalCountOfHours) // Якщо товар у гарному стані і його доставлено вчасно
+            {
+                driverBonus = (Program.rd.NextDouble() / 2.0) * 100; // Нараховується бонус для водія
+                fine = 0; // Значення штрафу встановлюється як 0
+            }
+            else if (cargoСondition < 72) // Якщо ж товар у поганому стані 
+            {
+                fine = Program.rd.NextDouble() * cargoAssessment * ((100 - cargoСondition) / 100.0); // Штраф напряму залежить від стану товару
+                driverBonus = 0; // Значення бонуса для водія встановлюється як 0
+            }
+            else if (driveHours > optimalCountOfHours) // Якщо ж товар не доставлено вчасно
+            {
+                fine = ((Program.rd.NextDouble() * 2) * 100 * (driveHours - optimalCountOfHours)); 
+                driverBonus = 0; // Значення бонуса для водія встановлюється як 0 
+            }
+
+            netProfit = profit - costs - fine + driverBonus;
+        }
+        public override void Output() // Перевизначення методу для виводу інформації
+        {
+            base.Output(); // Виклик методу базового класу для відображення базової інформації
+            Console.WriteLine("   Вантажний автомобіль перевозив {0} кг товару дорогою, що зайняла {1} з допустимих {3} год." +
+                "\n   Прибуток з перевезення: {4} грн.\n   Витрати під час перевезння: {5}\n   Оцінка товару, що перевозився: {6} грн." +
+                "\n   На момент прибуття стан товару оцінювався як: [{2}%/100%]", cargoWeight, driveHours, cargoСondition, optimalCountOfHours, profit, costs, cargoAssessment);
+            if (driverBonus != 0) // Якщо водій заробив штраф, то виводиться наступне повідомлення
+            {
+                Console.WriteLine("   Винагода, що отримав перевізник: {0:F2} грн.", driverBonus);
+            }
+            else if (fine != 0) // Якщо водій не заробив штраф, то виводиться наступне повідомлення
+            {
+                Console.WriteLine("   Штраф, що отримав перевізник: {0:F2} грн.", fine);
+            }
+        }
+    }
+
+    class PassengerTransport : Transport // Клас для представлення пасажирського автомобіля
+    {
+        // Приватні поля, що доступні тільки в даному класу
+        private int availableSeats; // Поле "Доступні місця"
+        private int numberOfPeople; // Поле "Кількість пасажирів"
+        private double driversRate; // Поле "Ставка водія"
+        private double distance; // Поле "Відстань"
+        private bool passengerSatisfaction; // Поле "Вдоволеність пасажирів"
+
+        public override void Input() // Перевизначення методу для вводу інформації
+        {
+            base.Input(); // Виклик методу з базового класу для вводу базової інформації
+            availableSeats = Program.IntegerInput(0, 60, "Введіть кількість місць в автобусі: ");
+            numberOfPeople = Program.IntegerInput(0, availableSeats, "Введіть кількість людей, що їдуть в путівку: ");
+            driversRate = Program.DoubleInput(0, 100000, "Введіть ставку водія (грн.): ");
+            distance = Program.DoubleInput(0, 10000000, "Введіть кілометраж поїздки (км. - в один бік): ");
+        }
+
+        protected override void CalculateNetProfit() // Перевизначення методу для обчислення чистого прибутку
+        {
+            double passengerMood = Program.rd.NextDouble(); // Генерування випадкового значення від 0 до 1
+            if (passengerMood >= 0.7) // Якщо настрій пасажирів переважно позитивний
+            {
+                passengerSatisfaction = true; // Встановлення значення true
+            }
+            else // Якщо ж настрій переважно негативний
+            {
+                passengerSatisfaction = false; // Встановлення значення false
+            }
+            if (!passengerSatisfaction) // Якщо пасажири невдоволені, то обчислюється чистий прибуток разом із заробленим штрафом - 100 грн за кожну людину
+            {
+                netProfit = profit - costs - driversRate + (0.2 * 2 * distance) - (100 * numberOfPeople);
+            }
+            else // Якщо ж людям все сподобалося, то обчислюється чистий прибуток без нарахування штрафів
+            {
+                netProfit = profit - costs - driversRate + (0.2 * 2 * distance);
+            }
+        }
+
+        public override void Output() // Перевизначення методу для виведення інформації
+        {
+            base.Output(); // Виклик методу з базового класу для виводу базової інформації
+            Console.WriteLine("   Пасажирський автомобіль перевозив {0} людей дорогою довжиною в {1} км." +
+                "\n   Прибуток з перевезення: {2} грн.\n   Витрати під час перевезння: {3}" +
+                "\n   Загальна кількість міcць в автобусі: {4}", numberOfPeople, 2 * distance, profit, costs, availableSeats);
+            if (passengerSatisfaction) // Якщо пасажири вдоволені поїздкою, то виводиться наступне повідомлення
+            {
+                Console.WriteLine("   Більшість пасажирів були задоволені поїзкою!");
+            }
+            else // Якщо не вдоволені
+            {
+                Console.WriteLine("   [УВАГА] - Більшість пасажирів були невдоволені поїздкою! (Штраф за кожну людину - 100 грн.)");
+            }
+            Console.WriteLine("   Ставка водія: {0}", driversRate);
+        }
+    }
+    internal class Program // Основний клас програми
+    {
+        public static Random rd = new Random(); // Створення посилання на об'єкт класу Random
+        public static int IntegerInput(double min, double max, string text) // Метод для валідації вводу цілого числа
+        {
+            text = text.Trim();
+            int number;
+            Console.Write("{0} ", text);
+            while (true) // Цикл, що триває поки користувач не введе число у правильному форматі 
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    while (number < min || number > max) // Цикл, що триває поки користувач не введе число, що належить заданому діапазону
+                    {
+                        if (number < min)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто мале число для цього параметра!\n\n{0} ", text);
+                        }
+                        else if (number > max)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто велике число для цього параметра!\n\n{0} ", text);
+                        }
+                        number = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                catch (FormatException) // Перехоплення помилки вводу
+                {
+                    Console.Write("\n[ПОМИЛКА] - Спробуйте ввести ще раз: ");
+                    continue;
+                }
+                return number; // Повернення цілого числа
+            }
+        }
+        public static double DoubleInput(double min, double max, string text) // Метод для валідації вводу числа з плаваючою комою
+        {
+            text = text.Trim();
+            double number;
+            Console.Write("{0} ", text);
+            while (true) // Цикл, що триває поки користувач не введе число у правильному форматі 
+            {
+                try
+                {
+                    number = Convert.ToDouble(Console.ReadLine());
+                    while (number < min || number > max) // Цикл, що триває поки користувач не введе число, що належить заданому діапазону
+                    {
+                        if (number < min)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто мале число для цього параметра!\n\n{0} ", text);
+                        }
+                        else if (number > max)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто велике число для цього параметра!\n\n{0} ", text);
+                        }
+                        number = Convert.ToDouble(Console.ReadLine());
+                    }
+                }
+                catch (FormatException) // Перехоплення помилки вводу
+                {
+                    Console.Write("\n[ПОМИЛКА] - Спробуйте ввести ще раз: ");
+                    continue;
+                }
+                return number; // Повернення числа
+            }
+        }
+        static void Main(string[] args) // Головний метод програми
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Встановлення кодування для коректного виводу українських символів в консоль
+            Console.InputEncoding = System.Text.Encoding.UTF8; // Встановлення кодування для коректного вводу українських символів в консоль
+
+            uint iterFreight = 0;
+            uint iterPassenger = 0;
+            int i;
+
+            int transportCount = Program.IntegerInput(1, 100, "Введіть кількість транспортних засобів: ");
+            Transport[] trp = new Transport[transportCount]; // Оголошення масиву об'єктів класу Transport
+            for (i = 0; i < trp.Length; i++) // Цикл, що проходиться по всім елементам масиву
+            {
+                Console.WriteLine();
+                int userChoice = Program.IntegerInput(1, 2, "[УВАГА] - Оберіть вид транспорту, про якого хочете заповнити інформацію:\n" +
+                "1 - Вантажний транспорт;\n2 - Пасажирський транспорт.\n\nВведіть ваш вибір: ");
+                switch (userChoice) // Вибір транспорту
+                {
+                    case 1:
+                        trp[i] = new FreightTransport(); // Створення нового посилання на об'єкт класу FreightTransport
+                        iterFreight++;
+                        Console.WriteLine("\n--- ВАНТАЖНИЙ ТРАНСПОРТ #{0} ---\n", iterFreight);
+                        trp[i].Input(); // Введення інформації про об'єкт
+                        break;
+                    case 2:
+                        trp[i] = new PassengerTransport(); // Створення нового посилання на об'єкт класу PassengerTransport
+                        iterPassenger++;
+                        Console.WriteLine("\n--- ПАСАЖИРСЬКИЙ ТРАНСПОРТ #{0} ---\n", iterPassenger);
+                        trp[i].Input(); // Введення інформації про об'єкт
+                        break;
+                }
+            }
+            Console.WriteLine("\n[УВАГА] - Інформацію про транспортні засоби успішно занесено!\n" +
+                "\nНатисність клавішу Enter, щоб вивести результати про наявні транспортні засоби...");
+            Console.ReadLine(); // Очікування натискання клавіші Enter
+            Console.Clear(); // Очищення консолі для гарного виводу кінцевих результатів
+            Console.WriteLine("\n~~~ РЕЗУЛЬТАТИ ~~~");
+              
+            for (i = 0; i < trp.Length; i++) // Цикл, що проходиться по всім елементам масиву об'єктів
+            {
+                Console.WriteLine("\n--- ТРАНСПОРТНИЙ ЗАСІБ #{0} ---", i + 1); 
+                trp[i].Output(); // Виведення інформації на екран про кожен об'єкт окремо 
+            }
+            Console.WriteLine("\nНатисніть Enter для завершення програми...");
+            Console.ReadLine(); // Очікування натискання клавіші Enter
+            Console.WriteLine("[УВАГА] - Завершення програми...");
+        }
+    }
+}
---
+++ lab.9/lab.9.2.cs
@@ -0,0 +1,247 @@
+﻿using System; // Використання простору імен System
+
+namespace lab._9._2
+{
+    class Student // Базвоий клас, що описує студента
+    {
+        protected string group; // Поле "група"
+        protected string name; // Поле "ім'я"
+        protected int course; // Поле "курс навчання"
+        protected int numberOfDisciplines; // Поле "кількість вивчаємих дисциплін"
+        protected bool scholarshipOpportunity; // Поле "можливість отримати стипендію"
+        protected string faculty; // Поле "факультет"
+
+        public virtual void Input() // Базовий метод для вводу даних про студента
+        {
+            Console.Write("Введіть ім'я студента: ");
+            name = Console.ReadLine();
+            Console.Write("Введіть назву факультету, на якому навчається студент: ");
+            faculty = Console.ReadLine();
+            Console.Write("Введіть групу студента: ");
+            group = Console.ReadLine();
+            course = Program.IntegerInput(1, 7, "Введіть на якому курсі навчається студент:");
+            numberOfDisciplines = Program.IntegerInput(1, 12, "Введіть кількість дисциплін, що вивчає студент: ");
+        }
+
+        protected virtual void ScholarshipAward() // Базовий метод для отримання стипендії
+        {
+            if (scholarshipOpportunity) // Якщо студент має можливість отриувати стипендію
+            {
+                Console.WriteLine("\n[УВАГА] - Треба більше інформації про студента для нарахування стипендії!");
+            }
+            else // Якщо ж не має такої можливості
+            {
+                Console.WriteLine("\n[УВАГА] - Студенту '{0}' стипендія не передбачена!", name);
+            }
+        }
+
+        public virtual void Output() // Базовий метод для відображення даних про студента
+        {
+            ScholarshipAward();
+            Console.WriteLine("\nІм'я студента: {0}\nФакультет: {1}\nГрупа: {2}\nКурс навчання: {3}" +
+                "\nКількість дисциплін, що вивчає студент: {4}", name, faculty, group, course, numberOfDisciplines);
+        }
+    }
+
+    class StateFundedStudent : Student // Похідний клас, що описує бюджетника
+    {
+        private double averageScore; // Поле "середній бал"
+        private int ratingsPlace; // Поле "місце в рейтингу"
+        private double scholarship; // Поле "стипендія"
+        private bool becomePayer = false; // Поле "стати платником"
+
+        public override void Input() // Перевизначений метод для вводу інформації про студента
+        {
+            base.Input(); // Виклик методу Input з базового класу
+            averageScore = Program.DoubleInput(5, 100, "Введіть середній бал студента: ");
+            ratingsPlace = Program.IntegerInput(1, 60, "Вкажіть, яке місце займає студент в рейтингу: ");
+        }
+
+        protected override void ScholarshipAward() // Перевизначений метод для отримання стипендії
+        {
+            if (averageScore >= 90 && ratingsPlace < 3) // Якщо середній бал вище за 80 і рейтингове місце менше 20-го
+            {
+                scholarshipOpportunity = true; // Встановлюється, що студент має стипендію
+                scholarship = 2999.99; // Нарахування стипендії у розмірі 2000
+            }
+            else if (averageScore >= 80 && ratingsPlace <= 20) // Якщо ж середній бал вище 90 і рейтингове місце менше 3-го
+            {
+                scholarshipOpportunity = true; // Встановлюється що студент має стипендію
+                scholarship = 2000; // Нарахування підвищеної стипендії
+            }
+            else if (averageScore < 80 && averageScore >= 60) // Якщо ж середній бал студента більше за 60, але менший ща 80
+            {
+                scholarshipOpportunity = false; // Студент не зможе отримувати стипендію
+            }
+            else if (averageScore < 60) // Якщо ж середній бал менший за 60
+            {
+                becomePayer = true; // Студент стає платником
+            }
+        }
+
+        public override void Output() // Перевизначений метод для виводу інформації про студента на екран
+        {
+            base.Output(); // Виклик методу Output з базового класу
+            Console.WriteLine("Студент '{0}', що навчається на бюджеті по всім предметам має середній бал: {1}" +
+                "\nСтудент займає {2} місце в рейтинговому списку на стипендію!", name, averageScore, ratingsPlace);
+            if (becomePayer)
+            {
+                Console.WriteLine("[УВАГА] - Студент не склав екзамени успішно і наступного семестру переходить на навчання за контрактом!");
+            }
+            else if (scholarshipOpportunity)
+            {
+                Console.WriteLine("Студент '{0}' наступні півроку отримуватиме стипендію у розмірі {1} грн!", name, scholarship);
+            }
+            else
+            {
+                Console.WriteLine("[УВАГА] - Даний студент навчатиметься на бюджеті, але стипендію на протязі наступного півріччя не отримає!");
+            }
+        }
+    }
+
+    class ContractStudent : Student // Похідний клас, що описує контратника
+    {
+        private double contractValue; // Поле "сума контракту"
+        private double debt; // Поле "борг"
+        private int daysLeft; // Поле "днів лишилося"
+
+        public override void Input() // Перевизначений метод для вводу інформації про студента
+        {
+            base.Input(); // Виклик методу Input з базвого класу
+            contractValue = Program.DoubleInput(4000, 200000, "Введіть суму навчання за контрактом (грн.): ");
+            debt = Program.DoubleInput(0, contractValue, "Введіть суму боргу по навчанню: ");
+            if (debt != 0) // Якщо є борг, то вводиться крайній термін для його погашення
+            {
+                daysLeft = Program.IntegerInput(0, 365, "Введіть кількість днів, що залишилася для оплати боргу (грн): ");
+            }
+        }
+
+        public override void Output() // Перевизначення методу для виведення інформації про студента на екран
+        {
+            base.Output(); // Виклик методу Output з базвого класу
+            Console.WriteLine("Студент платить за навчання {0} грн.", contractValue);
+            if (debt != 0 && daysLeft == 0) // Якщо навяний протермінований борг
+            {
+                Console.WriteLine("[УВАГА] - Студент не зміг вчасно оплатити навчання!\nДоля студента залишається невідомою...");
+            }
+            else if (debt != 0 && daysLeft != 0) // Якщо борг є, але ще не крайній термін дял його виплати
+            {
+                Console.WriteLine("[НАГАДУВАННЯ] - Студент має сплатити борг за навчання у розмірі {0} грн.\n[Часу, що лишилося: {1} дн.]", debt, daysLeft);
+            }
+            else if (debt == 0) // Якщо борг закрито
+            {
+                Console.WriteLine("[ВІТАННЯ] - Студент вчасно оплатив навчання та може сміливо продовжуватися навчатися на улюбленому факультеті {0}!", faculty);
+            }
+        }
+    }
+
+    internal class Program // Основний клас програми
+    {
+        public static int IntegerInput(double min, double max, string text) // Метод для валідації вводу цілого числа
+        {
+            text = text.Trim();
+            int number;
+            Console.Write("{0} ", text);
+            while (true) // Цикл, що триває поки користувач не введе число у правильному форматі 
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    while (number < min || number > max) // Цикл, що триває поки користувач не введе число, що належить заданому діапазону
+                    {
+                        if (number < min)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто мале число для цього параметра!\n\n{0} ", text);
+                        }
+                        else if (number > max)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто велике число для цього параметра!\n\n{0} ", text);
+                        }
+                        number = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                catch (FormatException) // Перехоплення помилки вводу
+                {
+                    Console.Write("\n[ПОМИЛКА] - Спробуйте ввести ще раз: ");
+                    continue;
+                }
+                return number; // Повернення цілого числа
+            }
+        }
+        public static double DoubleInput(double min, double max, string text) // Метод для валідації вводу числа з плаваючою комою
+        {
+            text = text.Trim();
+            double number;
+            Console.Write("{0} ", text);
+            while (true) // Цикл, що триває поки користувач не введе число у правильному форматі 
+            {
+                try
+                {
+                    number = Convert.ToDouble(Console.ReadLine());
+                    while (number < min || number > max) // Цикл, що триває поки користувач не введе число, що належить заданому діапазону
+                    {
+                        if (number < min)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто мале число для цього параметра!\n\n{0} ", text);
+                        }
+                        else if (number > max)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто велике число для цього параметра!\n\n{0} ", text);
+                        }
+                        number = Convert.ToDouble(Console.ReadLine());
+                    }
+                }
+                catch (FormatException) // Перехоплення помилки вводу
+                {
+                    Console.Write("\n[ПОМИЛКА] - Спробуйте ввести ще раз: ");
+                    continue;
+                }
+                return number; // Повернення числа
+            }
+        }
+        static void Main(string[] args) // Головний метод програми
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Налаштування кодування для коректного виводу українських символів в консоль
+            Console.InputEncoding = System.Text.Encoding.UTF8; // Налаштування кодування для коректного вводу українських символів в консоль
+
+            int i;
+
+            int studentCount = Program.IntegerInput(1, 100, "Введіть кількість студентів: ");
+            Student[] trp = new Student[studentCount]; // Оголошення масиву об'єктів класу Student
+            for (i = 0; i < trp.Length; i++) // Цикл, що проходиться по всім елементам масиву
+            {
+                Console.WriteLine();
+                int userChoice = Program.IntegerInput(1, 2, "[УВАГА] - Оберіть вид студента, про якого хочете заповнити інформацію:\n" +
+                "1 - Бюджетник;\n2 - Платник.\n\nВведіть ваш вибір: ");
+                switch (userChoice) // Вибір студента
+                {
+                    case 1:
+                        Console.WriteLine("\n--- СТУДЕНТ #{0} ---\n", i + 1);
+                        trp[i] = new StateFundedStudent(); // Створення нового посилання на об'єкт класу StateFundedStudent
+
+                        trp[i].Input(); // Введення інформації про об'єкт
+                        break;
+                    case 2:
+                        Console.WriteLine("\n--- СТУДЕНТ #{0} ---\n", i + 1);
+                        trp[i] = new ContractStudent(); // Створення нового посилання на об'єкт класу ContractStudent
+                        trp[i].Input(); // Введення інформації про об'єкт
+                        break;
+                }
+            }
+            Console.WriteLine("\n[УВАГА] - Інформацію про студентів успішно занесено!\n" +
+                "\nНатисність клавішу Enter, щоб вивести результати про студентів...");
+            Console.ReadLine(); // Очікування натискання клавіші Enter
+            Console.Clear(); // Очищення консолі для гарного виводу кінцевих результатів
+            Console.WriteLine("\n~~~ РЕЗУЛЬТАТИ ~~~");
+
+            for (i = 0; i < trp.Length; i++) // Цикл, що проходиться по всім елементам масиву об'єктів
+            {
+                Console.WriteLine("\n--- СТУДЕНТ #{0} ---", i + 1);
+                trp[i].Output(); // Виведення інформації на екран про кожен об'єкт окремо 
+            }
+            Console.WriteLine("\nНатисніть Enter для завершення програми...");
+            Console.ReadLine(); // Очікування натискання клавіші Enter
+            Console.WriteLine("[УВАГА] - Завершення програми...");
+        }
+    }
+}
