e26095ba135ea039f844fd94684656884a4309a8
Ivan-r-jpg <matadormax23@gmail.com>
2026-05-12 23:18:26 +0300

    Додавання файлів 12-ї лабораторної роботи

---
+++ lab.12/lab.12.1.cs
@@ -0,0 +1,84 @@
+﻿using System; // Підключення простору імен System
+using System.IO; // Підключення простору імен System.IO для роботи з файловими потоками
+
+namespace lab._12._1
+{
+    internal class Program // Основний клас програми
+    {
+        double DoubleInput(string text) // Метод для валідації вводу числа з плаваючою комою
+        {
+            text = text.Trim();
+            double number;
+            Console.Write("{0} ", text);
+            while (true) // Цикл, що триває поки користувач не введе число у правильному форматі 
+            {
+                try
+                {
+                    number = Convert.ToDouble(Console.ReadLine());  
+                }
+                catch (FormatException) // Перехоплення помилки вводу
+                {
+                    Console.Write("\n[ПОМИЛКА] - Спробуйте ввести ще раз: ");
+                    continue;
+                }
+                return number; // Повернення числа
+            }
+        }
+        static void Main(string[] args) // Головний метод програми
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Встановлення кодування UTF-8
+            double xMin, xMax, dx, k; // Оголошення змінних для збереження зчитуваних даних
+            StreamReader sr = new StreamReader("C:\\Users\\ivan-\\Desktop\\Visual_Studio\\lab.12.1\\lab.12.1\\FileInput.txt"); // Створення об'єкту файлового потоку для читання даних
+            StreamWriter sw = new StreamWriter("C:\\Users\\ivan-\\Desktop\\Visual_Studio\\lab.12.1\\lab.12.1\\FileOutput.txt", true); // Створення об'єкту файлового потоку для занесення даних
+            Console.WriteLine("Дано функцію: z = ((1 / tg(k * x)^1/3)) + cos(k * x) / ln(sin(k * x))");
+            Console.WriteLine();
+            Program ob = new Program(); // Створення посилання на клас Program
+            k = ob.DoubleInput("\nВведіть значення k:");
+            
+            string s; // Оголошення посередника
+
+            // Читання з файлу FileInput.txt
+            s = sr.ReadLine();
+            xMin = Convert.ToDouble(s);
+            s = sr.ReadLine();
+            dx = Convert.ToDouble(s);
+            s = sr.ReadLine();
+            xMax = Convert.ToDouble(s);
+
+            if (xMin > xMax && dx > 0) // Перевірка, чи зчитанні дані коректні
+            {
+                Console.WriteLine("\n[ПОМИЛКА] - Зчитане мінімальне значення з файлу більше за максимальне!");
+                return;
+            }
+            else if (xMax > xMin && dx < 0) // Якщо ж ні, то програма виводить відповідне повідомлення і завершує роботу
+            {
+                Console.WriteLine("\n[ПОМИЛКА] - Задано від'ємний крок при тому, що мінімальне значення менше за максимальне!");
+                return;
+            }
+            else // Вивід на екран повідомлення про успішне зчитування даних
+            {
+                Console.WriteLine("\n[УВАГА] - Дані з файлу успішно зчитано!");
+            }
+
+
+            sw.WriteLine("\nk = {0}\nMin: {1}\nMax: {2}\ndx = {3}", k, xMin, xMax, dx); // Запис інформації у файл FileOutput.txt
+            for (double x = xMin; x <= xMax; x+=dx)
+            {
+                if (Math.Tan(k * x) != 0 && Math.Sin(k * x) > 0 && Math.Log(Math.Sin(k * x)) != 0) // Перевірка ОДЗ
+                {
+                    double z = Math.Sign(1.0 / Math.Tan(k * x)) * Math.Pow(Math.Abs(1.0 / Math.Tan(k * x)), 1.0 / 3.0) + ((Math.Cos(k * x)) / Math.Log(Math.Sin(k * x)));
+                    Console.WriteLine("\n[УВАГА] - Результат функції: {0:F3}\nПри x = {1}", z, x);
+                    sw.WriteLine("\n[УВАГА] - Результат функції: {0:F3}\nПри x = {1}", z, x);
+                }
+                else
+                {
+                    Console.WriteLine("\n[УВАГА] - Помилка в обчисленні при х = {0}!", x);
+                    sw.WriteLine("\n[УВАГА] - Помилка в обчисленні при х = {0}!", x);
+                }
+            }
+
+            sr.Close(); // Закриття файлу FileInput.txt
+            sw.Close(); // Закриття файлу FileOutput.txt
+        }
+    }
+}
---
+++ lab.12/lab.12.2.cs
@@ -0,0 +1,115 @@
+﻿using System; // Підключення простору імен System
+using System.IO; // Підключення простору імен System.IO для роботи з файловими потоками
+
+namespace lab._12._2
+{
+
+    internal class Program // Основний клас програми
+    {
+        void OutputMatrix(double[,] arr, int rows, int cols, StreamWriter file) // Метод, що виводить матрицю на екран та записує її у файл
+        {
+            for(int i = 0; i < rows; i++)
+            {
+                for(int j = 0; j < cols; j++)
+                {
+                    Console.Write("{0,4}\t", arr[i, j]);
+                    file.Write("{0,4}\t", arr[i, j]);
+                }
+                Console.WriteLine("");
+                file.WriteLine("");
+            }
+        }
+        void ReadMatrix(double[,] matrix, int rows, int cols, StreamReader file) // Метод, що зчитує матрицю з файлу
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    string s = file.ReadLine();
+                    matrix[i, j] = Convert.ToDouble(s);
+                }
+            }
+        }
+        int IntegerInput(double min, double max, string text) // Метод для валідації вводу цілого числа
+        {
+            text = text.Trim();
+            int number;
+            Console.Write("{0} ", text);
+            while (true) // Цикл, що триває поки користувач не введе число у правильному форматі 
+            {
+                try
+                {
+                    number = Convert.ToInt32(Console.ReadLine());
+                    while (number < min || number > max) // Цикл, що триває поки користувач не введе число, що належить заданому діапазону
+                    {
+                        if (number < min)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто мале число для цього параметра!\n\n{0} ", text);
+                        }
+                        else if (number > max)
+                        {
+                            Console.Write("\n[УВАГА] - Ви ввели занадто велике число для цього параметра!\n\n{0} ", text);
+                        }
+                        number = Convert.ToInt32(Console.ReadLine());
+                    }
+                }
+                catch (FormatException) // Перехоплення помилки вводу
+                {
+                    Console.Write("\n[ПОМИЛКА] - Спробуйте ввести ще раз: ");
+                    continue;
+                }
+                return number; // Повернення цілого числа
+            }
+        }
+        static void Main(string[] args) // Головний метод програми
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8; // Встановлення кодування консолі UTF-8
+
+            StreamReader sr = new StreamReader("C:\\Users\\ivan-\\Desktop\\Visual_Studio\\lab.12.2\\lab.12.2\\MatrixInput.txt"); // Створення об'єкту файлового потоку для читання даних
+            StreamWriter sw = new StreamWriter("C:\\Users\\ivan-\\Desktop\\Visual_Studio\\lab.12.2\\lab.12.2\\Result.txt", true); // Створення об'єкту файлового потоку для запису даних
+
+            Program ob = new Program(); // Створення посилання на клас Program
+
+            // Введення рядків та стовпців матриці
+            int n = ob.IntegerInput(2, 1000, "Введіть кількість рядків матриці: "); 
+            int m = ob.IntegerInput(2, 1000, "Введіть кількість стовпців матриці: ");
+
+            // Оголошення двовимірних масивів
+            double[,] arr1 = new double[n, m];
+            double[,] arr2 = new double[m, n];
+            double[,] result = new double[n, n];
+
+            // Читання матриць з файлу
+            ob.ReadMatrix(arr1, n, m, sr);
+            ob.ReadMatrix(arr2, m, n, sr);
+            Console.WriteLine("\n[УВАГА] - Матриця arr1:\n");
+            sw.WriteLine("\n[УВАГА] - Матриця arr1:\n");
+            ob.OutputMatrix(arr1, n, m, sw);
+            Console.WriteLine("\n[УВАГА] - Матриця arr2:\n");
+            sw.WriteLine("\n[УВАГА] - Матриця arr2:\n");
+            ob.OutputMatrix(arr2, m, n, sw);
+
+            // Множення матриць та занесення результату в result
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < m; k++)
+                    {
+                        sum += arr1[i, k] * arr2[k, j];
+                    }
+                    result[i, j] = sum;
+                }
+            }
+            Console.WriteLine("\n[УВАГА] - Матриця result:\n");
+            sw.WriteLine("\n[УВАГА] - Результат множення обох матриць:\n");
+            ob.OutputMatrix(result, n, n, sw);
+            sw.WriteLine("\n------------------------------------------------\n");
+
+            // Закриття файлів
+            sr.Close();
+            sw.Close();
+        }
+    }
+}
