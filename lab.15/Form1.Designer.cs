3c26ffcb639e613a566ba87a06e3c47509403dba
Ivan-r-jpg <matadormax23@gmail.com>
2026-05-16 23:26:39 +0300

    Додавання файлів коду програми 15-ї лабораторної роботи

---
+++ lab.15/Form1.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace lab._15
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.groupBox5 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.groupBox5.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Items.AddRange(new object[] {
+            "Встала весна, чорну землю",
+            "Сонну розбудила,",
+            "Уквітчала її рястом,",
+            "Барвінком укрила;",
+            "І на полі жайворонок,",
+            "Соловейко в гаї",
+            "Землю, убрану весною,",
+            "Вранці зустрічають…"});
+            this.listBox1.Location = new System.Drawing.Point(17, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(503, 225);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 26);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(242, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(239, 48);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Підрахувати кількість символьних рядків ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Тип виділеного елементу:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 81);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(342, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(290, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Введіть символьний рядок, номер якого хочете знайти:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(15, 98);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(349, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 124);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 35);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Знайти";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(17, 70);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(135, 33);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Додати ще один рядок";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(333, 31);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(187, 33);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Видалити обраний рядок";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(17, 44);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(349, 20);
+            this.textBox4.TabIndex = 10;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(303, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Введіть символьний рядок, який хочете додати до списку:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(17, 31);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(310, 33);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Очистити список";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(333, 89);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(187, 40);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Видалити рядок за значенням";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(158, 70);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(208, 33);
+            this.button7.TabIndex = 14;
+            this.button7.Text = "Додати згенерований масив рядків";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(15, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(377, 125);
+            this.groupBox1.TabIndex = 15;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "РЕЗУЛЬТАТИ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 84);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(224, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Введіть символьний рядок для видалення:";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(17, 100);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(310, 20);
+            this.textBox5.TabIndex = 17;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.textBox5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.button6);
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Location = new System.Drawing.Point(407, 299);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(538, 139);
+            this.groupBox2.TabIndex = 18;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "СЕКТОР ВИДАЛЕННЯ";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.listBox1);
+            this.groupBox3.Location = new System.Drawing.Point(407, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(538, 269);
+            this.groupBox3.TabIndex = 19;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "СПИСОК РЯДКІВ";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.button7);
+            this.groupBox4.Controls.Add(this.label3);
+            this.groupBox4.Controls.Add(this.textBox4);
+            this.groupBox4.Controls.Add(this.button3);
+            this.groupBox4.Location = new System.Drawing.Point(15, 322);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(377, 116);
+            this.groupBox4.TabIndex = 20;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "СЕКТОР ДОДАВАННЯ";
+            // 
+            // groupBox5
+            // 
+            this.groupBox5.Controls.Add(this.button2);
+            this.groupBox5.Controls.Add(this.textBox3);
+            this.groupBox5.Controls.Add(this.label2);
+            this.groupBox5.Controls.Add(this.button1);
+            this.groupBox5.Location = new System.Drawing.Point(17, 143);
+            this.groupBox5.Name = "groupBox5";
+            this.groupBox5.Size = new System.Drawing.Size(374, 175);
+            this.groupBox5.TabIndex = 21;
+            this.groupBox5.TabStop = false;
+            this.groupBox5.Text = "СЕКТОР АНАЛІТИКИ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 450);
+            this.Controls.Add(this.groupBox5);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.groupBox5.ResumeLayout(false);
+            this.groupBox5.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.GroupBox groupBox5;
+    }
+}
+
---
+++ lab.15/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace lab._15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = "Тут відображатимуться результати...";
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button6.Enabled = false;
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                button4.Enabled = true;
+                textBox1.Text = listBox1.SelectedItem.ToString();
+                System.Type w = listBox1.SelectedItem.GetType();
+                textBox2.Text = w.ToString();
+
+            }
+            else
+            {
+                button4.Enabled = false;
+                textBox1.Text = "Тут відображатимуться результати...";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int count = listBox1.Items.Count;
+            textBox1.Text = "К-сть символьних рядків: " + count.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bool found = listBox1.Items.Contains(textBox3.Text);
+            if (found)
+            {
+                MessageBox.Show("Рядок знайдено!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Рядок не знайдено!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox1.Text = "Тут відображатимуться результати...";
+                textBox3.Clear();
+                textBox3.Focus();
+            }
+            int index = listBox1.Items.IndexOf(textBox3.Text);
+            if (index != -1)
+            {
+                textBox3.Clear();
+                textBox1.Text = "Номер шуканого рядка: " + (index + 1).ToString();
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            if(!string.IsNullOrEmpty(textBox3.Text))
+            {
+                button2.Enabled = true;
+            }
+            else
+            {
+                button2.Enabled = false;
+            }
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox4.Text))
+            {
+                button3.Enabled = true;
+            }
+            else
+            {
+                button3.Enabled = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int index = 0;
+            if(listBox1.SelectedIndex != -1)
+            {
+                index = listBox1.SelectedIndex;
+            }
+            listBox1.Items.Insert(index, textBox4.Text);
+            textBox4.Clear();
+            MessageBox.Show("Рядок успішно додано!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            textBox4.Focus();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.RemoveAt(listBox1.SelectedIndex);
+            textBox2.Clear();
+            MessageBox.Show("Рядок успішно видалено!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            bool found = listBox1.Items.Contains(textBox5.Text);
+            if (found)
+            {
+                listBox1.Items.Remove(textBox5.Text);
+                MessageBox.Show("Рядок успішно видалено!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                textBox5.Clear();
+                textBox5.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Такого рядка для видалення не знайдено!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            MessageBox.Show("Всі рядки успішно видалено!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            string[] items = { "Тарас Шевченко", "'Гайдамаки'", "7 квітня", "1841 року" };
+            listBox1.Items.AddRange(items);
+            MessageBox.Show("Рядки успішно додано!", "УВАГА", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBox5.Text))
+            {
+                button6.Enabled = true;
+            }
+            else
+            {
+                button6.Enabled = false;
+            }
+        }
+    }
+}
