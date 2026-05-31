090c22425ab51d0d4ffe5bffbb3438f7218b32d2
Ivan-r-jpg <matadormax23@gmail.com>
2026-05-31 23:26:31 +0300

    Додавання файлів коду лабораторної роботи №19

---
+++ lab.19/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace text_editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.fileToolStripMenuItem.Text = "Файл";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "Відкрити";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveAsToolStripMenuItem.Text = "Зберегти як";
+            this.saveAsToolStripMenuItem.Click += new System.EventHandler(this.saveAsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Вийти";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(46, 62);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(704, 376);
+            this.textBox1.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ lab.19/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Text;
+using System.IO; // Підключення простору імен для роботи з файлами
+using System.Windows.Forms;
+
+namespace text_editor
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath = ""; // Змінна для зберігання шляху до файлу
+
+        public Form1() // Конструктор форми
+        {
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e) // Обробник кнопки "Відкрити"
+        {
+            if (!AskToSaveChanges()) // Якщо користувач скасував дію, то відкриття файлу не виконується
+            { 
+                return; 
+            }
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK) // Якщо користувач вибрав файл і підтвердив відкриття
+            {
+                currentFilePath = openFileDialog1.FileName;
+
+                textBox1.Text = File.ReadAllText(currentFilePath, Encoding.UTF8); // Зчитування всього тексту із вибраного файлу
+
+                textBox1.Modified = false; // Після відкриття файл ще не змінений
+
+                this.Text = "Текстовий редактор - " + Path.GetFileName(currentFilePath); // Встановлення оновленої назви для форми 
+                
+                saveFileDialog1.FileName = currentFilePath; // Збереження шляху до вибраного файлу
+            }
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e) // Обробник натискання кнопки "Зберегти як"
+        {
+            SaveTextAs(); // Виклик методу для збереження файлу
+        }
+
+        private bool SaveTextAs()
+        {
+            if (!string.IsNullOrEmpty(currentFilePath)) // Якщо файл уже був відкритий, то пропонується збереження під тим самим іменем
+            {
+                saveFileDialog1.FileName = currentFilePath; // Збереження шляху до вибраного файлу
+            }
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK) // Якщо користувач натиснув "Ок" при збереженні файла
+            {
+                currentFilePath = saveFileDialog1.FileName; // Збереження шляху до цього файлу
+
+                File.WriteAllText(currentFilePath, textBox1.Text, Encoding.UTF8); // Запис тексту із textBox1 у файл
+
+                textBox1.Modified = false; // Після збереження змін уже немає
+
+                this.Text = "Текстовий редактор - " + Path.GetFileName(currentFilePath); // Встановлення оновленої назви для форми
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e) // Обробник кнопки "Вийти"
+        {
+            this.Close(); // Закриття форми
+        }
+
+        private void Form1_Load(object sender, EventArgs e) // Завантаження форми
+        {
+            this.Text = "Текстовий редактор"; // Встановлення назви форми
+
+            openFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Встановлення пропонованих та доступних розширень для відкриття файлів
+            saveFileDialog1.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*"; // Встановлення пропонованих та доступних розширень для збереження файлів
+
+            saveFileDialog1.DefaultExt = "txt"; // Встановлення стандартного розширення для файлів
+            saveFileDialog1.AddExtension = true; // Встановлення можливості автоматично додавати розширення навіть якщо користувач його не вказав
+            saveFileDialog1.OverwritePrompt = true; // Запитує, чи замінити файл, якщо він уже існує
+
+            textBox1.Modified = false; // При запуску форми текст ще не змінювався
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e) // Процес закриття форми
+        {
+            if (!AskToSaveChanges())  // Якщо користувач скасував дію, то закриття форми скасовується
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private bool AskToSaveChanges() // Метод, що запитує чи зберегти внесені зміни
+        {
+            if (textBox1.Modified == false) // Якщо текстове поле не було змінене
+            { 
+                return true; 
+            }
+
+            DialogResult result = MessageBox.Show(
+                "Текст був змінений. Зберегти зміни?",
+                "Закрити вікно",
+                MessageBoxButtons.YesNoCancel,
+                MessageBoxIcon.Question
+            ); // Виклик діалогового вікна
+
+            if (result == DialogResult.No) // Якщо користувач натиснув "Ні"
+            { 
+                return true; 
+            }
+
+            if (result == DialogResult.Cancel) // Якщо користувач натиснув "Відмінити"
+            { 
+                return false; 
+            }
+
+            if (result == DialogResult.Yes) // Якщо користувач натиснув "Так"
+            { 
+                return SaveTextAs(); // Виклик методу для збереження файлу
+            }
+
+            return true;
+        }
+    }
+}
